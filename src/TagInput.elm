dcd6be8d605cfde2d2f34e03a08f5fc50293dba8
NicholasKennedy <kennedyn@ca.ibm.com>
2015-12-16 12:22:00 -0400

    Factored out parts of view

--- src/TagInput.elm
+++ src/TagInput.elm
@@ -3,27 +3,35 @@ module TagInput where
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
+import Signal exposing (..)
 import StartApp.Simple as StartApp
 import Json.Decode as Json
 
 
-type alias Tag = { id: Int, text: String}
-type alias Model = { tags: List Tag , inputText: String }
-type Action = Edit String | Add | Remove Int | NoOp
+type alias Tag = { id : Int,
+                   text : String
+                 }
+type alias Model = { tags: List Tag,
+                     inputText: String
+                   }
+type Action
+     = NoOp
+     | Add
+     | Edit String
+     | Remove Int
+
 
 model: Model
-model = {
-        tags = []
-        , inputText = ""
-      }
+model = { tags = [],
+          inputText = ""
+        }
 
 
 update: Action -> Model -> Model
 update action model =
   case action of
 
-    Edit string ->
-       { model | inputText = string }
+    NoOp -> model
 
     Add ->
       let
@@ -36,12 +44,27 @@ update action model =
             inputText = ""
         }
 
+    Edit string ->
+       { model | inputText = string }
+
     Remove id ->
       { model | tags = List.filter (\tag -> tag.id /= id) model.tags }
 
-    NoOp ->
-      model
 
+view: Address Action -> Model -> Html
+view address model =
+  div [] (tagInput address model.inputText :: tagList address model.tags)
+
+
+tagInput: Address Action -> String -> Html
+tagInput address inputText =
+    input [ onEnter address,
+            onEdit address Edit,
+            value inputText] []
+
+tagList: Address Action -> List Tag -> List Html
+tagList address tags =
+  List.map (tagToHtml address) tags
 
 isEnter: Int -> Action
 isEnter keycode =
@@ -51,29 +74,22 @@ isEnter keycode =
     _ ->
       NoOp
 
-onEdit: Signal.Address Action -> (String -> Action) -> Attribute
+onEdit: Address Action -> (String -> Action) -> Attribute
 onEdit address value =
   on "input" targetValue (\str -> Signal.message address (value str))
 
-onEnter: Signal.Address Action -> Attribute
+onEnter: Address Action -> Attribute
 onEnter address =
   on "keydown" keyCode (\key -> Signal.message address (isEnter key))
 
-onRemove: Signal.Address Action -> Int -> Attribute
+onRemove: Address Action -> Int -> Attribute
 onRemove address id =
     onClick address (Remove id)
 
-tagToHtml: Signal.Address Action -> Tag -> Html
+tagToHtml: Address Action -> Tag -> Html
 tagToHtml address tag =
     div [onRemove address tag.id] [text tag.text]
 
-
-view: Signal.Address Action -> Model -> Html
-view address model =
-  div [] (input [ onEdit address Edit,
-                  onEnter address,
-                  value model.inputText] []
-                   :: List.map (tagToHtml address) model.tags )
 
 main =
     StartApp.start { model = model, update = update, view = view }
